7dc5ea642c707ff1e9e2aea1820cd795ae2e0842
redparry <red_parry@yahoo.co.uk>
2021-08-20 23:42:08 +0100

    added the basic code for updating the wifi settings

---
+++ helper_functions/wifi_connection.py
@@ -0,0 +1,76 @@
+def do_connect(ssid, password, hostname):
+    import network
+    from time import sleep
+    sta_if = network.WLAN(network.STA_IF)
+    max_attempts = 100
+    attempts = 0
+
+    if not sta_if.isconnected():
+        sta_if.active(True)
+        sta_if.config(dhcp_hostname=hostname)
+        sta_if.connect(ssid, password)
+        while not sta_if.isconnected():
+            if attempts > max_attempts:
+                break
+            else:
+                sleep(5)
+                attempts += 1
+    print('network config:', sta_if.ifconfig())
+    return sta_if.isconnected()
+
+
+def load_wifi_settings(config_file):
+    import json
+    with open(config_file, 'rb') as f:
+        return json.load(f)
+
+
+def connect_wifi(config_file):
+    wireless_properties = load_wifi_settings(config_file)
+    return do_connect(**wireless_properties)
+
+
+def update_wifi_html(ssid, password, hostname):
+    with open("helper_functions/resources/update_wifi.html") as f:
+        text = f.read()
+    return text.format(ssid=ssid, password=password, hostname=hostname)
+
+
+def new_wifi_html(ssid, password, hostname):
+    with open("helper_functions/resources/new_wifi_settings.html") as f:
+        text = f.read()
+    return text.format(ssid=ssid, password=password, hostname=hostname)
+
+
+def send_response(conn, response):
+    conn.send('HTTP/1.1 200 OK\n')
+    conn.send('Content-Type: text/html\n')
+    conn.send('Connection: close\n\n')
+    conn.sendall(response)
+
+
+def update_wifi(config_file):
+    import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', 80))
+    s.listen(5)
+
+    wireless_properties = load_wifi_settings(config_file)
+    attempts = 0
+    max_attempts = 10
+    while attempts < max_attempts:
+        conn, addr = s.accept()
+        conn.settimeout(60.0)
+        request = conn.recv(1024)
+        conn.settimeout(None)
+        request = str(request)
+        print('GET Request Content = %s' % request)
+        if request.find('/update_wifi'):
+            # update json
+            wireless_properties = load_wifi_settings(config_file)
+            new_wifi_html(**wireless_properties)
+        else:
+            response = update_wifi_html(**wireless_properties)
+            send_response(conn, response)
+        conn.close()
+        attempts += 1
