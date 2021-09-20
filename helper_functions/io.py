338a321dafeb445a1a07e3e8da1eef5e11182ce9
redparry <red_parry@yahoo.co.uk>
2021-09-20 21:35:10 +0100

    made the json update all values which are present instead of dumping the whole file

--- helper_functions/io.py
+++ helper_functions/io.py
@@ -4,7 +4,10 @@ def load_json_settings(config_file):
         return json.load(f)
 
 
-def update_json_settings(config_file, data):
+def update_json_settings(config_file, new_data):
     import json
+    with open(config_file, 'rb') as f:
+        data = json.load(f)
+    data.update(new_data)
     with open(config_file, 'wb') as f:
         json.dump(data, f)
