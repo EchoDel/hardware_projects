0a5515b405c1b0726837c25448f77c722f90a865
redparry <red_parry@yahoo.co.uk>
2021-09-21 10:20:22 +0100

    added function to update the properties

--- PlantMonitor/webpages.py
+++ PlantMonitor/webpages.py
@@ -1,8 +1,9 @@
 from PlantMonitor.helper_functions import get_colour
-from helper_functions.io import load_json_settings
+from helper_functions.io import load_json_settings, update_json_settings
 from helper_functions.soil_moisture import get_soil_moisture
 from helper_functions.temperature import get_temperature
 from helper_functions.wifi_connection import get_wireless_settings
+from tinyweb.server import parse_query_string
 
 
 # tinyweb server based classed instead of sockets
@@ -70,6 +71,29 @@ def setup_tinyweb_soil_moisture(app, plant_config_file):
     async def favicon(request, response):
         await response.send_file("PlantMonitor/resources/favicon.ico",
                                  content_type="image/x-icon")
+
+    @app.route('/update_temperature')
+    async def update_temperature(request, response):
+        result = parse_query_string(request.query_string.decode())
+        new_config = {}
+        new_config['temperature'] = {key: float(value)
+                                     for key, value in result.items()}
+        update_json_settings(plant_config_file, new_config)
+        await response.start_html()
+        # Send actual HTML page
+        await response.send('<meta http-equiv="Refresh" content="0; url=\'/index\'" />')
+
+    @app.route('/update_soil')
+    async def update_soil(request, response):
+        result = parse_query_string(request.query_string.decode())
+        new_config = {}
+        new_config['soil_moisture'] = {key: float(value)
+                                       for key, value in result.items()}
+        update_json_settings(plant_config_file, new_config)
+        await response.start_html()
+        # Send actual HTML page
+        await response.send(
+            '<meta http-equiv="Refresh" content="0; url=\'/index\'" />')
 
     app.add_resource(GetStatus,
                      '/get_status',
