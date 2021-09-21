33b438099523f366d81233ed1fc5b532b1c3afac
redparry <red_parry@yahoo.co.uk>
2021-09-21 11:10:12 +0100

    reformated to be nicer to read

--- PlantMonitor/webpages.py
+++ PlantMonitor/webpages.py
@@ -76,19 +76,25 @@ def setup_tinyweb_soil_moisture(app, plant_config_file):
     async def update_temperature(request, response):
         result = parse_query_string(request.query_string.decode())
         new_config = {}
-        new_config['temperature'] = {key: float(value)
-                                     for key, value in result.items()}
+        new_config['temperature'] = \
+            {key: float(value) for key, value in result.items()}
         update_json_settings(plant_config_file, new_config)
         await response.start_html()
         # Send actual HTML page
-        await response.send('<meta http-equiv="Refresh" content="0; url=\'/index\'" />')
+        await response.send(
+            '<meta http-equiv="Refresh" content="0; url=\'/index\'" />')
 
     @app.route('/update_soil')
     async def update_soil(request, response):
         result = parse_query_string(request.query_string.decode())
         new_config = {}
-        new_config['soil_moisture'] = {key: float(value)
-                                       for key, value in result.items()}
+        new_config['soil_moisture'] = \
+            {key: float(value) for key, value in result.items()}
+        update_json_settings(plant_config_file, new_config)
+        await response.start_html()
+        # Send actual HTML page
+        await response.send(
+            '<meta http-equiv="Refresh" content="0; url=\'/index\'" />')
         update_json_settings(plant_config_file, new_config)
         await response.start_html()
         # Send actual HTML page
